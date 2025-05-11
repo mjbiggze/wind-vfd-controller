7f8378efc333d9621f0fafd84aee78384f060af9
Maggiemoe@9942 <mjbiggze@gmail.com>
2025-05-10 23:59:45 -0500

    Add proper application files

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+import json
+import time
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+# Your complete Python code would go here
+print("Wind VFD Control System Initialized")
